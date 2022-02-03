867bb5d6976f433bb8c44b605714f8ac3370d4d8
katiemca <71445491+katiemca@users.noreply.github.com>
2022-02-03 11:31:05 +0000

    Update mainjs.js

--- mainjs.js
+++ mainjs.js
@@ -8,6 +8,9 @@ let el2 = document.querySelector('.bannertwo');
 let btn3 = document.querySelector('.nav-link3');
 let el3 = document.querySelector('.bannerthree');
 
+let btn4 = document.querySelector('.nav-link4');
+
+
 btn.addEventListener('click', function () {
     el.scrollIntoView({behavior: "smooth", block: "end", inline: "nearest"});
 });
@@ -18,6 +21,10 @@ btn2.addEventListener('click', function () {
 
 btn3.addEventListener('click', function () {
     el3.scrollIntoView({behavior: "smooth", block: "center", inline: "nearest"});
+});
+
+btn4.addEventListener('click', function () {
+    el.scrollIntoView({behavior: "smooth", block: "center", inline: "nearest"});
 });
 
 // hamburger
@@ -43,3 +50,34 @@ function closeMenu() {
     hamburger.classList.remove("active");
     navMenu.classList.remove("active");
 }
+
+// carousel
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
+
